f8d12cdabb45ff08847bca0f78ab1b154ae82544
zzzuuusurp <zg5e@student.cart.org>
2026-03-09 09:59:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-const careers = fetch(eecu-data-server.vercel.app/data)
+const overlay = document.getElementById("overlay");
+const careerTitle = document.getElementById("occupation");
+//copying rey's code for a bit
+function createButtons(careers) {
+    careers.forEach((career, index) => {
+     const button = document.createElement("button");
+     button.innerHTML = `${career.Occupation}: ${career.Salary}`;
+     button.setAttribute("id", `${index}`);
+        button.addEventListener("click", () => {
+            careerTitle.innerHTML = `Future Career: ${career.Occupation}`;
+            console.log(`Selected Career: ${career.Occupation}, Salary: ${career.Salary}`);
+        });
+        overlay.appendChild(button);
+    });
+}
+
+async function getCareers() {
+    const url = "https://eecu-data-server.vercel.app/data";
+    try {
+        const response = await fetch(url);
+        const jobs = await response.json();
+        createButtons(jobs); //see above
+        return jobs;
+    }
+    catch (error) {
+        console.error("Error fetching careers data:", error);
+        return [];
+    }
+    
+}
+getCareers();
